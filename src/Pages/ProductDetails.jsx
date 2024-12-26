19d9ec9b7f974b29e0f0dc41a7231011cf6aca06
Ronish29 <ronishparmar2003@gmail.com>
2024-12-26 12:11:42 +0530

    styling changed

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -46,60 +46,61 @@ const ProductDetails = () => {
     }
 
     return (
-        <div className="flex flex-col md:flex-row border p-4 max-w-4xl mx-auto mt-10">
-            <div className="flex justify-center md:w-1/2">
-                <div className="w-full flex justify-center">
-                    <LazyLoadImage
-                        alt={product.title}
-                        className={`max-w-full h-auto object-contain transition-all duration-300 ${
-                            imageLoaded ? 'blur-none' : 'blur-lg'
-                        }`}
-                        effect="blur"
-                        src={product.image}
-                        beforeLoad={() => setImageLoaded(false)}
-                        onLoad={handleImageLoad}
-                    />
-                </div>
-            </div>
-            <div className="md:w-1/2 md:pl-8 mt-4 md:mt-0">
-                <h1 className="text-2xl font-bold">{product.title}</h1>
-                <p className="text-gray-600 mt-2">Product ID: {product.id}</p>
+        <>
+            <h1 className="text-4xl font-bold text-blue-400 text-center mt-5">
+                Product Details
+            </h1>
+            {
+                isLoading ? <h1 className="text-center text-blue-500 mt-10">Loading...</h1>
+                :
+                (
+                    <div className="flex flex-col md:flex-row border p-4 max-w-4xl mx-auto mt-10">
+                        <div className="flex justify-center md:w-1/2">
+                            <div className="w-full flex justify-center">
+                                <LazyLoadImage
+                                    alt={product.title}
+                                    className={`transition-all duration-300 
+                                        ${imageLoaded ? 'blur-none' : 'blur-lg'}`}
+                                    effect="blur"
+                                    src={product.image}
+                                    beforeLoad={() => setImageLoaded(false)}
+                                    onLoad={handleImageLoad}
+                                />
+                            </div>
+                        </div>
+                        <div className="md:w-1/2 md:pl-8 mt-4 md:mt-0">
+                            <h1 className="text-2xl font-bold">{product.title}</h1>
+                            <p className="text-gray-600 mt-2">Product ID: {product.id}</p>
+                            <div className="mt-4">
+                                <h2 className="text-xl font-semibold">Description</h2>
+                                <p className="text-gray-700 mt-2">{product.description}</p>
+                            </div>
 
-                <div className="mt-4">
-                    <h2 className="text-xl font-semibold">Description</h2>
-                    <p className="text-gray-700 mt-2">{product.description}</p>
-                </div>
+                            <div className="mt-4">
+                                <h2 className="text-xl font-semibold">Category</h2>
+                                <p className="text-gray-700 mt-2">{product.category}</p>
+                            </div>
 
-                <div className="mt-4">
-                    <h2 className="text-xl font-semibold">Category</h2>
-                    <p className="text-gray-700 mt-2">{product.category}</p>
-                </div>
+                            <div className="mt-4">
+                                <h2 className="text-xl font-semibold">Rating</h2>
+                                    <p className="text-gray-700 mt-2">Rate: {product.rating.rate}</p>
+                                </div>
 
-                <div className="mt-4">
-                    <h2 className="text-xl font-semibold">Rating</h2>
-                    {product.rating ? (
-                        <p className="text-gray-700 mt-2">Rate: {product.rating.rate}</p>
-                    ) : (
-                        <p className="text-gray-700 mt-2">No ratings available</p>
-                    )}
-                </div>
+                            <div className="mt-4">
+                                <h2 className="text-xl font-semibold">Reviews</h2>
+                                    <p className="text-gray-700 mt-2">Count: {product.rating.count}</p>
+                                </div>
 
-                <div className="mt-4">
-                    <h2 className="text-xl font-semibold">Reviews</h2>
-                    {product.rating ? (
-                        <p className="text-gray-700 mt-2">Count: {product.rating.count}</p>
-                    ) : (
-                        <p className="text-gray-700 mt-2">No reviews available</p>
-                    )}
-                </div>
-
-                <div className="mt-4">
-                    <h2 className="text-xl font-semibold">
-                        Price: <span className="text-blue-600">${product.price.toFixed(2)}</span>
-                    </h2>
-                </div>
-            </div>
-        </div>
+                            <div className="mt-4">
+                                <h2 className="text-xl font-semibold">
+                                    Price: <span className="text-blue-600">${product.price.toFixed(2)}</span>
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </>
     );
 };
 
